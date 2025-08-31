5091d26ecb8c23dce1897cb17e5cc3384d93699b
karase28 <jkopacz@os.pl>
2025-08-31 20:46:26 +0200

    pierwszy

---
+++ __init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# Ten moduł nie zawiera kodu Pythona, więc plik __init__.py może być pusty.
---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'WZ - Adres Produkcji',
+    'license': 'LGPL-3',
+    'version': '18.0.1',
+    'summary': 'Dodaje adres produkcji do raportu WZ',
+    'author': 'Jarosław Kopacz',
+    'depends': ['stock'],
+    'data': [
+        'views/raport_wz_dziedziczacy_po_oryg.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
